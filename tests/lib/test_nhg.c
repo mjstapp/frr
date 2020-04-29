7c71425b7dd76140f9092418e3621d380b478cf3
Mark Stapp <mjs@voltanet.io>
2020-04-29 13:39:16 -0400

    tests: add NHG unit test module
    Add a module to do nexthop-group api tests.
    
    Signed-off-by: Mark Stapp <mjs@voltanet.io>

---
+++ tests/lib/test_nhg.c
@@ -0,0 +1,24 @@
+/*
+ * Nexthop group objects tests.
+ * This tests the nexthop group and nexthop objects.
+ *
+ * Copyright (C) 2020 by Volta Networks Inc.
+ *
+ * This file is part of FreeRangeRouting (FRR).
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <zebra.h>
---
+++ tests/lib/test_nhg.py
@@ -0,0 +1,4 @@
+import frrtest
+
+class TestNexthopIter(frrtest.TestMultiOut):
+    program = './test_nhg'
